4ac9276501b375b3dc8efb4b872917d84e4a59b2
Andreas Siegwarth <andreassiegwarth@startmail.com>
2024-06-04 15:31:57 +0200

    added input validation for ip address and subnet mask, using regular expressions (but needs refactoring)

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <regex.h>
 #include "ipv4netid.h"
 #define IP_ADDR_SEGMENTS 4
 #define BINARY_DIGITS 8
@@ -19,8 +20,41 @@ int subnetdecimal[IP_ADDR_SEGMENTS];
 
 int main(int argc, char **argv)
 {
-    printf("Enter IP address: ");
-    scanf("%s", &ipstring);
+    regex_t regex;
+    int regexcheck = regcomp(&regex, "^[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}$", REG_EXTENDED);
+    int rangecheck = -1;
+
+    do {
+        printf("Enter IP address: ");
+        scanf("%s", ipstring);
+
+        regexcheck = regexec(&regex, ipstring, 0, NULL, 0);
+        if (0 == regexcheck) {
+            splitipstring(ipstring, ipchars);
+            convertipstring(ipchars, ipdecimal);
+            for (int i = 0; i < 4; i++) {
+                printf("%d ", ipdecimal[i]);
+            }
+            if ((0 <= ipdecimal[0] && ipdecimal[0] <= 255)
+                && (0 <= ipdecimal[1] && ipdecimal[1] <= 255)
+                && (0 <= ipdecimal[2] && ipdecimal[2] <= 255)
+                && (0 <= ipdecimal[3] && ipdecimal[3] <= 255)) {
+                rangecheck = 0;
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        ipchars[i][j] = '\0';
+                    }
+                }
+                for (int i = 0; i < 4; i++) {
+                    ipdecimal[i] = 0;
+                }
+                printf("Sorry, address out of range!\n");
+            }
+        } else {
+                printf("Sorry, wrong format!\n");
+        }
+    } while (rangecheck != 0);
 
     splitipstring(ipstring, ipchars);
 
@@ -28,8 +62,39 @@ int main(int argc, char **argv)
 
     tobinary(ipdecimal, ipbinary);
 
-    printf("Enter subnet mask: ");
-    scanf("%s", &subnetstring);
+    rangecheck = -1;
+
+    do {
+        printf("Enter subnet mask: ");
+        scanf("%s", subnetstring);
+
+        regexcheck = regexec(&regex, subnetstring, 0, NULL, 0);
+        if (0 == regexcheck) {
+            splitipstring(subnetstring, subnetchars);
+            convertipstring(subnetchars, subnetdecimal);
+            for (int i = 0; i < 4; i++) {
+                printf("%d ", subnetdecimal[i]);
+            }
+            if ((0 <= subnetdecimal[0] && subnetdecimal[0] <= 255)
+                && (0 <= subnetdecimal[1] && subnetdecimal[1] <= 255)
+                && (0 <= subnetdecimal[2] && subnetdecimal[2] <= 255)
+                && (0 <= subnetdecimal[3] && subnetdecimal[3] <= 255)) {
+                rangecheck = 0;
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        subnetchars[i][j] = '\0';
+                    }
+                }
+                for (int i = 0; i < 4; i++) {
+                    subnetdecimal[i] = 0;
+                }
+                printf("Sorry, address out of range!\n");
+            }
+        } else {
+                printf("Sorry, wrong format!\n");
+        }
+    } while (rangecheck != 0);
 
     splitipstring(subnetstring, subnetchars);
 
